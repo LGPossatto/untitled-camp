623f655f2ec49cdb5a183c466e3d85a33effc1b0
Luiz Gustavo <lgpossatto@gmail.com>
2022-05-24 19:06:04 -0300

    added spinner to products showcase container

--- frontend/src/components/containers/products-showcase/ProductsShowcase.tsx
+++ frontend/src/components/containers/products-showcase/ProductsShowcase.tsx
@@ -2,8 +2,9 @@ import { useContext, useEffect } from "react";
 
 import { ProductsContext } from "../../../context/products/ProductsContext";
 
-import { ProductCard } from "../../cards/product-card/ProductCard";
 import "./products-showcase.scss";
+import { ProductCard } from "../../cards/product-card/ProductCard";
+import { Spinner } from "../../visual-elements/spinner/Spinner";
 
 export const ProductsShowcase = () => {
   const { randomProducts, getRandomlProducts } = useContext(ProductsContext);
@@ -14,10 +15,15 @@ export const ProductsShowcase = () => {
 
   return (
     <section className="products-showcase flex ai-c">
-      {randomProducts.length >= 9 &&
+      {randomProducts.length > 0 ? (
         randomProducts.map((product, i) => (
           <ProductCard key={i} product={product} stock={false}></ProductCard>
-        ))}
+        ))
+      ) : (
+        <div className="products-showcase__spinner flex jc-c">
+          <Spinner></Spinner>
+        </div>
+      )}
     </section>
   );
 };
