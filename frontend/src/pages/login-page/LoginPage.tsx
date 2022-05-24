da036d7b0366de8169261e76f6f857a0a9d30e04
Luiz Gustavo <lgpossatto@gmail.com>
2022-05-24 15:26:41 -0300

    added alert to login page, minify imgs

--- frontend/src/pages/login-page/LoginPage.tsx
+++ frontend/src/pages/login-page/LoginPage.tsx
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
+
 import "./login-page.scss";
 import { Login } from "../../components/containers/login/Login";
 
 export const LoginPage = () => {
+  useEffect(() => {
+    window.alert(
+      "If you don't want to create an account, you can enter using this instead:\n\nEmail: john@email.com\nPassword: john123"
+    );
+  }, []);
+
   return (
     <div className="login-page flex jc-c container">
       <Login></Login>
